1e35b20aa35d53e33ec318cd944862d05af4d06f
Isabel <ivolkfel@asu.edu>
2025-10-13 21:12:41 -0700

    added health page

---
+++ public/js/auth.js
@@ -0,0 +1,12 @@
+// List of pages that do NOT require authentication
+// const publicPages = ['login.html', 'create-account.html'];
+
+// Get current page filename
+// const currentPage = window.location.pathname.split('/').pop();
+
+// if (!publicPages.includes(currentPage)) {
+//  const token = localStorage.getItem('token');
+//  if (!token) {
+//    window.location.href = 'login.html';
+// }
+// }
